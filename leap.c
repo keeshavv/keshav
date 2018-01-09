13c076f89ae58ffda4942bcbca3dbbfe6a3caa0c
Keshav Chaudhary <keshav.chaudhary18@gmail.com>
2018-01-10 01:02:50 +0530

    Add files via upload

---
+++ leap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+    int y;
+    scanf("%d",&y);
+    if(y%4 == 0)
+    {
+        if( y%100 == 0)
+        {
+            if ( y%400 == 0)
+                printf("yes");
+            else
+                printf("no");
+        }
+        else
+            printf("yes");
+    }
+    else
+        printf("no");
+    
+}
